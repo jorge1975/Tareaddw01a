16b793113f45870b6c091b5696c5d3a336f865aa
Administrador <Administrador@192.168.113.73>
2019-08-23 18:12:51 -0500

    Commit 1. Realizando pruebas

---
+++ src/senati/ddw/git/Salud.java
@@ -0,0 +1,10 @@
+package senati.ddw.git;
+
+public class Salud {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Saludos cordiales");
+	}
+
+}
